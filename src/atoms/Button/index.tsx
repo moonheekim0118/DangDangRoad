6ba08b3eaba4b20e99a5e5439a5030e54f0aad84
moonee <uuuu118@naver.com>
2021-01-31 15:48:17 +0900

    feat : add Button Atoms

---
+++ src/atoms/Button/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+interface Props {
+  children: React.ReactNode;
+  color: 'blue' | 'white';
+  onClick?: (e: React.MouseEvent<HTMLSpanElement>) => void;
+}
+
+const Button = ({ children, color, onClick }: Props) => {
+  return (
+    <Container onClick={onClick} color={color}>
+      {children}
+    </Container>
+  );
+};
+
+const Container = styled.button<{ color: string }>`
+  background-color: ${(props) => (props.color === 'blue' ? '#0277bc' : '#fff')};
+  color: ${(props) => (props.color === 'blue' ? '#fff' : '#0277bc')};
+  font-size: 1.2rem;
+  border: none;
+  border-radius: 15px;
+  padding: 13px 15px;
+  cursor: pointer;
+
+  -webkit-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+  -moz-box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.25);
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export default Button;
