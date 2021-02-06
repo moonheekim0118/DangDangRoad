6ab88c7348fac500deab5accaadb56a419c7c31b
moonee <uuuu118@naver.com>
2021-02-06 17:36:32 +0900

    refactor : split logic and view

--- src/components/Forms/SignUpForm/index.tsx
+++ src/components/Forms/SignUpForm/index.tsx
@@ -2,22 +2,45 @@ import React from 'react';
 import Input from '../../../atoms/Input';
 import Button from '../../../atoms/Button';
 import styled from '@emotion/styled';
-import useSignUp from '../../../hooks/useSignUp';
 import { inputId } from '../../../model/inputIds';
 
-const SignUpForm = () => {
-  const [
-    email,
-    emailError,
-    EmailChangeHandler,
-    password,
-    passwordError,
-    PasswordChangeHandler,
-    passwordCheck,
-    passwordMatch,
-    PasswordCheckChangeHandler,
-    SubmitHanlder,
-  ] = useSignUp();
+type handlerFunction = (e: React.ChangeEvent<HTMLInputElement>) => void;
+
+interface Props {
+  /** email value */
+  email: string;
+  /** email error */
+  emailError: boolean;
+  /** email Change Handler function */
+  EmailChangeHandler: handlerFunction;
+  /** password value */
+  password: string;
+  /** password error  */
+  passwordError: boolean;
+  /** password change Handler function */
+  PasswordChangeHandler: handlerFunction;
+  /** double check password value*/
+  passwordCheck: string;
+  /** double check password error  */
+  passwordMatch: boolean;
+  /** double check password change handler function */
+  PasswordCheckChangeHandler: handlerFunction;
+  /** submit handler function */
+  SubmitHanlder: (e: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+const SignUpForm = ({
+  email,
+  emailError,
+  EmailChangeHandler,
+  password,
+  passwordError,
+  PasswordChangeHandler,
+  passwordCheck,
+  passwordMatch,
+  PasswordCheckChangeHandler,
+  SubmitHanlder,
+}: Props) => {
   return (
     <Form>
       <Input
--- src/pages/signUp.tsx
+++ src/pages/signUp.tsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import useSignUp from '../hooks/useSignUp';
 import SignUpForm from '../components/Forms/SignUpForm';
 
 const SignUp = () => {
+  /** logic */
+  const [
+    email,
+    emailError,
+    EmailChangeHandler,
+    password,
+    passwordError,
+    PasswordChangeHandler,
+    passwordCheck,
+    passwordMatch,
+    PasswordCheckChangeHandler,
+    SubmitHanlder,
+  ] = useSignUp();
+
   return (
     <Layout>
-      <SignUpForm />
+      <SignUpForm
+        email={email}
+        emailError={emailError}
+        EmailChangeHandler={EmailChangeHandler}
+        password={password}
+        passwordError={passwordError}
+        PasswordChangeHandler={PasswordChangeHandler}
+        passwordCheck={passwordCheck}
+        passwordMatch={passwordMatch}
+        PasswordCheckChangeHandler={PasswordCheckChangeHandler}
+        SubmitHanlder={SubmitHanlder}
+      />
     </Layout>
   );
 };
