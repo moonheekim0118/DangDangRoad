7b0842b5354a2e334ea99f853cd4b700f9694210
moonee <uuuu118@naver.com>
2021-02-07 10:26:31 +0900

    fix : change error text

--- src/remotes/signUp.ts
+++ src/remotes/signUp.ts
@@ -1,16 +1,24 @@
 import firebase from '../../firebase';
 
+const errorExTxt = (errorCode) => {
+  let result = '';
+  switch (errorCode) {
+    case 'auth/email-already-in-use':
+      result = '이미 사용중인 이메일 입니다';
+      return result;
+    default:
+      result = '잠시후 다시 시도해주세요';
+      return result;
+  }
+};
+
 const signUp = async (email, password) => {
   try {
     const auth = firebase.auth();
     const user = await auth.createUserWithEmailAndPassword(email, password);
     return { isError: false, errorMessage: '' };
   } catch (error) {
-    const errorMessage =
-      error.message ===
-      'The email address is already in use by another account.'
-        ? '이미 사용중인 이메일 입니다.'
-        : '잠시후에 다시 시도해주세요.';
+    const errorMessage = errorExTxt(error.code);
     return { isError: true, errorMessage };
   }
 };
