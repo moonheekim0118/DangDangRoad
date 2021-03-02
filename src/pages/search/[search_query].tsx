596ce37d1bdc50eddf3f0783f6b85d781f861ecf
moonee <uuuu118@naver.com>
2021-03-03 00:37:49 +0900

    feat : add getReviewByKeyword API

--- src/pages/search/[search_query].tsx
+++ src/pages/search/[search_query].tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-
-const SearchResult = () => {};
+import React, { useEffect } from 'react';
+import api from 'api';
+const SearchResult = () => {
+  useEffect(() => {
+    api.getReviewByKeyword('광명').then((result) => {});
+  }, []);
+  return <div>후후</div>;
+};
 
 export default SearchResult;
