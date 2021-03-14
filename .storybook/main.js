66abe0e45791682ffbf26ebad097de6a469dc35e
moonee <uuuu118@naver.com>
2021-03-14 10:48:26 +0900

    chore : uninstall unneccesary modules from storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -18,13 +18,9 @@ module.exports = {
         {
           loader: require.resolve('babel-loader'),
           options: {
-            presets: [
-              ['react-app', { flow: false, typescript: true }],
-              require.resolve('@emotion/babel-preset-css-prop'),
-            ],
+            presets: [require.resolve('@emotion/babel-preset-css-prop')],
           },
         },
-        require.resolve('react-docgen-typescript-loader'),
       ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
