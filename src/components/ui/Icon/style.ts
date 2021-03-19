c0c1fa7cf640afbef9471ef6f3dcdbdc64cf340b
moonee <uuuu118@naver.com>
2021-03-19 19:39:38 +0900

    fix : icon-button styling error

--- src/components/ui/Icon/style.ts
+++ src/components/ui/Icon/style.ts
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const ButtonComponent = styled.button`
   border: 0;
-  background-color: inherit;
+  background-color: transparent;
   color: inherit;
 
   &:focus {
@@ -15,6 +15,7 @@ export const ButtonComponent = styled.button`
 export const Component = styled(FontAwesomeIcon)`
   color: inherit;
   width: 100%;
+  height: 100%;
 `;
 
 export const sizes = {
