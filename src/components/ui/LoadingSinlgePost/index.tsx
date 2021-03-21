c514fb1e780d41b11185eec0e7eac68220f6f78c
moonee <uuuu118@naver.com>
2021-03-21 11:45:55 +0900

    style : update loading svg

--- src/components/ui/LoadingSinlgePost/index.tsx
+++ src/components/ui/LoadingSinlgePost/index.tsx
@@ -8,11 +8,11 @@ const LoadingSinglePost = () => {
     <>
       <ContentLoader width="100%" height="100%" css={S.contentsLoaderStyle}>
         <rect x="5%" y="50" rx="0" ry="0" width="calc(100%/4)" height="80%" />
-        <rect x="35%" y="50" rx="0" ry="0" width="calc(100%/4)" height="80%" />
-        <rect x="75%" y="80" rx="4" ry="4" width="254" height="6" />
-        <rect x="76%" y="100" rx="3" ry="3" width="201" height="6" />
-        <circle cx="71%" cy="100" r="48" />
-        <rect x="71%" y="180" rx="0" ry="0" width="calc(100%/4)" height="30%" />
+        <circle cx="40%" cy="100" r="48" />
+        <rect x="35%" y="200" rx="0" ry="0" width="calc(100%/4)" height="50%" />
+        <rect x="45%" y="80" rx="4" ry="4" width="150" height="6" />
+        <rect x="46%" y="100" rx="3" ry="3" width="100" height="6" />
+        <rect x="67%" y="50" rx="0" ry="0" width="calc(100%/4)" height="80%" />
       </ContentLoader>
       <S.LoaderContainer>
         <Loading />
