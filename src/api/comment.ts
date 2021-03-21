a2f33b3c47308d677a14ef9aad0c51232d2f4a99
moonee <uuuu118@naver.com>
2021-03-21 13:43:59 +0900

    remove Comment api temporaly

--- src/api/comment.ts
+++ src/api/comment.ts
@@ -3,43 +3,43 @@ import * as T from 'types/API';
 import { COMMENT_DATA_LIMIT } from 'common/constant/number';
 import { getUserData } from 'api/review';
 
-//get comments by 5
+// //get comments by 5
 
-const extractCommentData = async (response): Promise<T.CommentData> => {
-  try {
-  } catch (error) {}
-};
+// const extractCommentData = async (response): Promise<T.CommentData> => {
+//   try {
+//   } catch (error) {}
+// };
 
-export const getComments = async (
-  postId: string,
-  key?: string
-): T.APIResponse<T.CommentResult> => {
-  try {
-    const response = await db
-      .collection('comments')
-      .where('postId', '==', postId)
-      .limit(COMMENT_DATA_LIMIT)
-      .get();
-    return { isError: false, data: '' };
-  } catch (error) {
-    throw error;
-  }
-};
+// export const getComments = async (
+//   postId: string,
+//   key?: string
+// ): T.APIResponse<T.CommentResult> => {
+//   try {
+//     const response = await db
+//       .collection('comments')
+//       .where('postId', '==', postId)
+//       .limit(COMMENT_DATA_LIMIT)
+//       .get();
+//     return { isError: false, data: '' };
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-// write comment
+// // write comment
 
-export const writeComment = async () => {
-  try {
-  } catch (error) {
-    throw error;
-  }
-};
+// export const writeComment = async () => {
+//   try {
+//   } catch (error) {
+//     throw error;
+//   }
+// };
 
-// remove comment
+// // remove comment
 
-export const removeComment = async () => {
-  try {
-  } catch (error) {
-    throw error;
-  }
-};
+// export const removeComment = async () => {
+//   try {
+//   } catch (error) {
+//     throw error;
+//   }
+// };
