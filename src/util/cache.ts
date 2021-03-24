1a526a65a4c40d35e9efeebf2f51130ece998a59
moonee <uuuu118@naver.com>
2021-03-24 14:08:04 +0900

    refactor : update function to class

--- src/util/cache.ts
+++ src/util/cache.ts
@@ -1,26 +1,21 @@
 const EXPIRE_TIME = 10000000;
 
-// To Expire Data after specific time
 interface CacheData<T> {
   value: T;
+  // To Expire Data after specific time
   expiry: number;
 }
 
-// Cache Interface Data Type
-interface CacheInterface<T> {
-  data: CacheData<T>[];
-  has: (key: string) => boolean;
-  get: (key: string) => T;
-  set: (key: string, value: T) => void;
-  delete: (key: string) => void;
-}
-
 // Prototype for Caching Data
-function cacheProto<T>(this: CacheInterface<T>) {
-  this.data = [];
+class cacheProto<T> {
+  private data: CacheData<T>[];
+
+  constructor() {
+    this.data = [];
+  }
 
   // check if there is Data with Cache Key
-  this.has = (key: string): boolean => {
+  has = (key: string): boolean => {
     if (this.data[key]) {
       if (this.data[key].expiry > new Date().getTime()) {
         return true;
@@ -31,12 +26,12 @@ function cacheProto<T>(this: CacheInterface<T>) {
   };
 
   // Get Value from Data by Its key
-  this.get = (key: string): T => {
+  get = (key: string): T => {
     return this.data[key].value;
   };
 
   // Set Value with Key
-  this.set = (key: string, value: T) => {
+  set = (key: string, value: T) => {
     this.data[key] = {
       value,
       expiry: new Date().getTime() + EXPIRE_TIME,
@@ -44,7 +39,7 @@ function cacheProto<T>(this: CacheInterface<T>) {
   };
 
   // Delete (G.C) Data by Key
-  this.delete = (key: string) => {
+  delete = (key: string) => {
     delete this.data[key];
   };
 }
