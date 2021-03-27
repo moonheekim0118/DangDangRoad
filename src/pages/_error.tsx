4787dd86b42adadd61f7d73923e7d3378215e3b5
moonee <hellomooneekim@gmail.com>
2021-03-27 16:42:09 +0900

    fix : update custom error pages

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -4,7 +4,7 @@ const Error = ({ statusCode }) => {
   return (
     <h1>
       {statusCode
-        ? '서버에 문제가 생겼습니다. 죄송합니다.'
+        ? `${statusCode}error. 서버에 문제가 생겼습니다. 죄송합니다.`
         : '존재하지 않는 페이지 입니다.'}
     </h1>
   );
