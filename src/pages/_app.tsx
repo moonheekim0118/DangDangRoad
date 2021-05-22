b104f1d54c54536726bc07d041a9b30acab41229
moonee <hellomooneekim@gmail.com>
2021-05-22 10:09:34 +0900

    chore : change favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,22 +26,18 @@ const App = ({ Component, pageProps }) => {
               href="https://fonts.googleapis.com/css2?family=Do+Hyeon&display=swap"
               rel="stylesheet"></link>
 
-            <link
-              rel="apple-touch-icon"
-              sizes="180x180"
-              href="/apple-touch-icon.png"
-            />
+            <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
             <link
               rel="icon"
               type="image/png"
               sizes="32x32"
-              href="/favicon-32x32.png"
+              href="/favicon.ico"
             />
             <link
               rel="icon"
               type="image/png"
               sizes="16x16"
-              href="/favicon-16x16.png"
+              href="/favicon.ico"
             />
             <link rel="manifest" href="/site.webmanifest" />
             <link
